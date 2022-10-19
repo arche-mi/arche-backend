1d43c8a747f22f63d65ef7a1292188db1cab2e30
yayahc <hamedcuenca5@gmail.com>
2022-10-19 13:25:15 +0000

    fetch user info from firebase api

--- arch/src/components/dashboard/Dashboard.js
+++ arch/src/components/dashboard/Dashboard.js
@@ -1,34 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
-import { doc, query, collection, getDocs, where } from "firebase/firestore";
-import { db, auth } from "../../firebase";
+import { auth } from "../../firebase";
 
 function Dashboard() {
-    const [user, loading, error] = useAuthState(auth);
+    const [user, loading] = useAuthState(auth);
     const navigate = useNavigate();
     const [name, setName] = useState("");
-
+    const creationTime = user.metadata.creationTime.split(',')[1].split('GMT');
+    const lastSeen = user.metadata.lastSignInTime.split(',')[1].split('GMT');
+    const photoUrl = user.photoURL;
+    
     // Fetch username by uid
     const fetchUserName = async () => {
         try {
             setName(user.displayName);
-            console.log("user id :" + user.uid);
+            console.log(user);
         } catch (err) {
             console.error(err);
         }
     }; 
-
+    
     useEffect(() => {
         if (loading) return;
         if (!user) return navigate("/sign");
             
         fetchUserName();
-    }, [user, loading]);
+    });
       
 
     return (
-        <p>Ton profil : {name}</p>
+        <div>
+            <img src={photoUrl} alt="user"/>
+            <p>{name}</p>
+            <p>Derniere connexion : {lastSeen}</p>
+            <p>Inscrit le : {creationTime}</p>
+        </div>
     )
 }
 
