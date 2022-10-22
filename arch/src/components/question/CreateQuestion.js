48b63b7d892f9723a85404f0fce850a9140582bd
yayahc <hamedcuenca5@gmail.com>
2022-10-22 04:04:45 +0000

    le c est passe pour les questions

--- arch/src/components/question/CreateQuestion.js
+++ arch/src/components/question/CreateQuestion.js
@@ -1,33 +1,91 @@
+import React, { useEffect, useState } from "react";
+import { auth,db } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { query, collection, getDocs, where } from "firebase/firestore";
+import { doc, setDoc, updateDoc, addDoc} from "firebase/firestore";
+import { isEmpty, map } from "@firebase/util";
 
-
-const fetchUserQuestions = async () => {
-    try {
-        //get user preview questions **************** 
-        const questions = {};
-        return questions;
-    } catch (error) {
-        console.log(error);
-    }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const create = async () => {
-    try {
-        const title = document.querySelector('#title').value;
-        const text = document.querySelector('#text').value;
-        const tags = document.querySelector('#title').value;        
-        let questions = fetchUserQuestions();
-        const date = new Date();
-        questions[0] = [title, text, tags, date];
+function CreateQuestion() {
+    const [user, loading] = useAuthState(auth);
+    const navigate = useNavigate();
+    const [name, setName] = useState();
+    const [questions, setQuestions] = useState();
 
-        //push new on firebase **************************
-        console.log(questions);
 
-    } catch (error) {
-        console.log(error);
+    const fetchUserName = async () => {
+        try {        
+          const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+          const doc = await getDocs(q);
+          const data = doc.docs[0].data();
+        
+          setName(data.name);
+        } catch (err) {
+          console.error(err);
+        }
+    }; 
+    
+    const fetchUserQuestions = async () => {
+        try {
+            const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+            const doc = await getDocs(q);
+            const data = doc.docs[0].data();
+            //await sleep(1000);
+            
+            if (!data.questions) {
+                const questions = {};                
+                data.questions = questions;
+            }
+            setQuestions(data.questions);
+        } catch (error) {
+            console.log(error);
+        }
     }
-};
 
-function createQuestion() {
+    const create = async () => {
+        try {       
+            const title = document.querySelector('#title').value;
+            const text = document.querySelector('#text').value;
+            const tags = document.querySelector('#title').value;    
+            if (title === '' || text === '' || tags === '') {
+                return false;
+            } else {               
+                const date = new Date();
+                let key = Object.keys(questions).length;
+                if (isEmpty(questions)) {
+                    setQuestions({});
+                    key = -1;
+                }
+                questions[key] = [{title:title}, {text:text}, {tags:tags}, date];
+                console.log(questions);
+
+                console.log(name);
+                const userDocByName = doc(db, "users", name);
+                await updateDoc(userDocByName, {
+                    questions: questions
+                });
+                window.location.reload();
+            }            
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+    useEffect(() => {
+        if (loading) return;
+        if (!user) return navigate("/sign");
+
+       fetchUserName();
+       fetchUserQuestions();
+    }, [user, loading]);
+
+    
     return (
         <div>
             <h2>Poser une question?</h2>
@@ -48,4 +106,4 @@ function createQuestion() {
     )
 }
 
-export default createQuestion;
+export default CreateQuestion;
