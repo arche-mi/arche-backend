f488306e38bab6a6a6839a24c374afc4a55f3931
yayahc <hamedcuenca5@gmail.com>
2022-11-07 02:32:58 +0000

    gros update sur les reponses

--- arch/src/components/question/ReadQuestion.js
+++ arch/src/components/question/ReadQuestion.js
@@ -4,10 +4,13 @@ import { useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { query, collection, getDocs, where } from "firebase/firestore";
 import { doc, updateDoc} from "firebase/firestore";
+import storage from "../../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { async, isEmpty } from "@firebase/util";
+
 
 
 // External fonctions
-
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -41,7 +44,15 @@ function ReadQuestion() {
     const [tags, setTags] = useState();
     const [date, setDate] = useState();
     const [questionPhoto, setQuestionPhoto] = useState(); 
-    const [responses, setResponses] = useState();
+    const [responses, setResponses] = useState("");
+
+     // State to store uploaded file
+    //const [file, setFile] = useState("");
+    const [fileUrl, setUrl] = useState("");
+    // progress
+    const [percent, setPercent] = useState(0);
+    const [isReady, setIsready] = useState(true);
+
 
     const questionId = window.location.href.split('?')[1].split('!')[0];
     const userid = window.location.href.split('!')[1].split('#')[0];
@@ -75,18 +86,26 @@ function ReadQuestion() {
             setTitle(Object.values(questions[questionId][0])[0]);
             setText(Object.values(questions[questionId][1])[0]);
             setQuestionPhoto(Object.values(questions[questionId][5]).join(''));
-            console.log(questionPhoto);
 
-            const tagsp = document.querySelector("#tags");
+             // Fetch users responses
+             let emptyRespData = {};
+             const userFetchResponses = Object.values(questions[questionId][3])[0];
+             console.log(userFetchResponses)
+             console.log(Object.keys(userFetchResponses).pop())
+             if (isEmpty(userFetchResponses)) {
+                 setResponses(emptyRespData);
+            } else {
+                setResponses(userFetchResponses)
+             }
+            
+            const resp = document.querySelector("#resp");
             for (let i=0; i < Object.values(questions[questionId][2])[0].length; i++) {
                 let ul = document.createElement("ul");
                 let li = document.createElement("li");
                 li.innerText = Object.values(questions[questionId][2])[0][i];
                 ul.appendChild(li);
-                tagsp.appendChild(ul);
-            }
-
-            //setResponses(Object.values(questions[questionId][3]));
+                resp.appendChild(ul);
+            }          
 
             const currentTime = Date.now();
             const fetchTime = questions[questionId][4].toDate();           
@@ -134,6 +153,98 @@ function ReadQuestion() {
         }
     }
 
+
+
+    const updateResponses = async () => {
+        let questions = null;        
+        try {
+            const q = query(collection(db, "users"), where("uid", "==", userid));
+            const doct = await getDocs(q);
+            const data = doct.docs[0].data();
+            questions = data.questions;
+            questions[questionId][3] = {responses:responses};
+            const userDocByUsername = doc(db, "users", name);
+            await updateDoc(userDocByUsername, {
+                questions: questions
+            });
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+
+    const createNewResponses = async () => {
+        console.log(responses)
+        if (isReady) {
+            try {       
+                const response_text = document.querySelector('#response_text').value;
+                if (response_text === '') {
+                    alert("remplissez le champs reponse svp");
+                    return false;
+                } else {               
+                    const date = new Date();
+                    let key = Object.keys(responses).length;
+                    if (isEmpty(responses)) {
+                        setResponses({});
+                        key = 0;
+                    }
+                    console.log(`On a deja ${key} reponses`);
+                    responses[key] = [{text:response_text}, {user:userid}, {user_answer:user?.uid}, date, fileUrl];
+
+                    updateResponses();                    
+                }            
+    
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            alert('Patienter le temps que le fichier se charge ...')
+        }        
+    };
+
+
+    const handleUpload = async (event) => {
+        const file = event.target.files[0];
+        if (!file) {
+            // alert("Please choose a file first!")
+            console.log('not file');
+            return false;
+        }
+     
+        const storageRef = ref(storage,`/photo/question/${file.name}`)
+        const uploadTask = uploadBytesResumable(storageRef, file);
+     
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const percent = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+     
+                // update progress
+                setPercent(percent);
+                if (percent != 100) {
+                    setIsready(false);
+                }
+            },
+            (err) => console.log(err),
+            () => {
+                // download url
+                getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+                    if (url) {
+                        console.log(url)
+                        setUrl(url);
+                        setIsready(true);
+                    }
+                });
+            }
+        ); 
+    }
+
+    
+
     function switchToProfile() {
         window.location = `/user?${name}#${userid}`;
     }
@@ -157,8 +268,22 @@ function ReadQuestion() {
                 <p id="tags">tags: </p>
                 <p>publier le : {date}</p>
                 <img src={questionPhoto} alt="Photo"/>
-                <p>reponses : {responses}</p>
-                <button onClick={deletQuestion}>supprimer</button>
+                <p id='resp'>reponses : </p>
+
+                <div>
+                    <label>
+                        Contenu de la reponse:
+                        <textarea id="response_text"></textarea>
+                    </label><br></br>
+                    <label>
+                        Photo:
+                        <input type="file" accept="/image/*" onChange={handleUpload}/>
+                        <p>{percent} "%"</p>
+                    </label>
+                    <button onClick={createNewResponses}>repondre</button>
+                </div>
+
+                <button onClick={deletQuestion}>supprimer la question</button>
             </div>
         )
     } else {
@@ -171,7 +296,21 @@ function ReadQuestion() {
                 <p id="tags">tags: </p>
                 <p>publier le : {date}</p>
                 <img src={questionPhoto} alt="Photo"/>
-                <p>reponses : {responses}</p>
+                <p id='resp'>reponses : </p>
+
+                <div>
+                    <label>
+                        Contenu de la reponse:
+                        <textarea id="response_text"></textarea>
+                    </label><br></br>
+                    <label>
+                        Photo:
+                        <input type="file" accept="/image/*" onChange={handleUpload}/>
+                        <p>{percent} "%"</p>
+                    </label>
+                    <button onClick={createNewResponses}>repondre</button>
+                </div>
+
             </div>
         )
     }
